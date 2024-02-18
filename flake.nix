ff705fa3df4e7aacdacd92648c60ffbaea4d9431
Heinrich Hartmann <derhein@gmail.com>
2024-02-18 19:55:26 +0000

    .

--- flake.nix
+++ flake.nix
@@ -4,25 +4,23 @@
     nixos-hardware.url = "github:nixos/nixos-hardware";
   };
   outputs = { self, nixpkgs, nixos-hardware }: rec {
-    nixosConfigurations = {
-      pi = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          nixos-hardware.nixosModules.raspberry-pi-4
-          "${nixpkgs}/nixos/modules/profiles/minimal.nix"
-          ./configuration.nix
-          ./base.nix
-        ];
-      };
+    system = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        nixos-hardware.nixosModules.raspberry-pi-4
+        "${nixpkgs}/nixos/modules/profiles/minimal.nix"
+        ./configuration.nix
+        ./base.nix
+      ];
     };
-    images = {
-      pi = (self.nixosConfigurations.pi.extendModules {
-        modules = [
-          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-          { disabledModules = [ "profiles/base.nix" ]; }
-        ];
-      }).config.system.build.sdImage;
-    };
+    image = (system.pi.extendModules {
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+        { disabledModules = [ "profiles/base.nix" ]; }
+      ];
+    }).config.system.build.sdImage;
+    nixosConfigurations = { pi = system; };
+    images = { pi = image; };
     packages.x86_64-linux.pi-image = images.pi;
     packages.aarch64-linux.pi-image = images.pi;
   };
