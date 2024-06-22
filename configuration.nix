f8c03ff8ea972514e631052ee09e2b323ba8a3cf
Hannes Graf <misterhannes@hotmail.com>
2024-06-22 11:45:09 +0200

    init

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,9 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILECy6PK+pg6QaEoKQ4sr32COh14nnEE5BdzmqOK13Ca rspi4@example.com"
   ];
 in { pkgs, config, lib, ... }: {
+imports = [
+  "${fetchTarball "https://github.com/NixOS/nixos-hardware/tarball/master"}/raspberry-pi/4"
+];
   system.stateVersion = "24.05";
   environment.systemPackages = with pkgs; [
     vim
@@ -57,6 +60,19 @@ in { pkgs, config, lib, ... }: {
     settings.PasswordAuthentication = true;
     settings.PermitRootLogin = "yes";
   };
+    time.timeZone = "Europe/Zurich"; # Time zone and Internationalisation
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      extraLocaleSettings = {
+        LC_TIME = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+      };
+    };
+
+    console = {
+      font = "Lat2-Terminus16";
+      keyMap = "sg";
+    };
 
   environment.etc."tmux.conf".source = ./tmux.conf;
 
