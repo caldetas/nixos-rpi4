c99af5e86cdad2eb562a512ff90518aaf88aefc4
Hannes Graf <misterhannes@hotmail.com>
2024-06-22 13:53:27 +0200

    plasma

--- configuration.nix
+++ configuration.nix
@@ -48,12 +48,12 @@ in { pkgs, config, lib, ... }: {
     services.xserver.displayManager.sddm.enable = true;
     services.xserver.desktopManager.plasma5.bigscreen.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
-#  networking.interfaces.wlan0 = {
-#    ipv4.addresses = [{
-#      address = "192.168.178.102";
-#      prefixLength = 24;
-#    }];
-#  };
+  networking.interfaces.wlan0 = {
+    ipv4.addresses = [{
+      address = "192.168.178.102";
+      prefixLength = 24;
+    }];
+  };
   networking.hostName = "nixos";
 
 ##  networking.defaultGateway = "192.168.178.1";
